019f03bb7df07148dca1f3f5722a571c39a7adbd
fearbites <fearbites@gmail.com>
2024-07-18 17:07:21 +0900

    NewAuth

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { bear, coin, highVoltage, notcoin, rocket, trophy } from './images';
 
 const App: React.FC = () => {
   const [points, setPoints] = useState(0);
-  const [energy, setEnergy] = useState(1000);
+  const [energy, setEnergy] = useState(0);
   const [click, setClick] = useState<{ id: number, x: number, y: number }[]>([]);
   const pointsToAdd = 12;
   const energyToReduce = 12;
